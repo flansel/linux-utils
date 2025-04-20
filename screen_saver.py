72568f4e5538edfec68e9ffbc5221362d27c761d
flansel <fjansell@gmail.com>
2025-04-20 12:38:16 -0400

    Use window names

--- screen_saver.py
+++ screen_saver.py
@@ -1,12 +1,13 @@
 import subprocess
 import argparse
 import os
+from typing import Tuple, Dict
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--script-name", help="Name of the output script (default screen-name)", default=None)
 args, unkown = parser.parse_known_args()
 
-def parse_w_line(line: str):
+def parse_w_line(line: str) -> Tuple[int, str]:
     cols = line.split()
     command = ' '.join(cols[7:])
     window_number = int(cols[2].split('.')[-1])
@@ -15,15 +16,17 @@ def parse_w_line(line: str):
 def running_in_screen() -> bool:
     return os.getenv("STY") is not None
 
-def get_window_names():
+def get_window_names() -> Dict[int, str]:
     res = subprocess.check_output(['screen', '-Q', 'windows'])
     res = res.split()
-    res = [res[i] for i in range(res) if i % 2 != 0]
+    res = {int(res[i]):res[i + 1] for i in range(0, res, 2)}
     return res
 
+def get_current_window() -> int:
+    res = subprocess.check_output(['screen', '-Q', 'number']).split()[0]
+    return int(res)
+
 def get_current_screen_name() -> str:
-    if not running_in_screen():
-        raise SystemError("Not running inside of a screen.")
     return os.getenv("STY")
 
 def save():
@@ -35,17 +38,20 @@ def save():
     # break up res into lines, remove the first line which is a header
     res = res.split('\\n')[2:-1]
     res = [parse_w_line(r) for r in res]
+    window_names = get_window_names()
     
     # build the bash script
     f = open(script_name, "w")
     f.write("#!/bin/sh\nscreen -dmS {0}\n".format(screen_name))
     for window, command in res:
         if window != 0:
-            f.write("screen -S test -X screen {0}\n".format(window))
-        f.write("screen -S test -p {0} -X stuff {1}\r\n".format(window, command))
+            f.write("screen -S {0} -X screen -t {1}\n".format(screen_name, window_names[window] if window in window_names else window))
+        f.write("screen -S {0} -p {1} -X stuff {2}\r\n".format(screen_name, window, command))
 
     f.close()
     os.chmod(script_name, 0o775)
 
 if __name__ == "__main__":
+    if not running_in_screen():
+        raise SystemError("screen-saver must be run inside of a GNU-screen.")
     save()
