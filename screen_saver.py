0331320f3519392181a3f80cd23ef9668177af20
flansel <fjansell@gmail.com>
2025-04-21 15:16:39 -0400

    use utmp

--- screen_saver.py
+++ screen_saver.py
@@ -1,18 +1,12 @@
 import subprocess
 import argparse
 import os
-import sys
+import utmp
 from typing import Tuple, Dict
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--script-name", help="Name of the output script (default screen-name)", default=None)
 args, unkown = parser.parse_known_args()
-
-def parse_w_line(line: str) -> Tuple[int, str]:
-    cols = line.split()
-    command = ' '.join(cols[7:])
-    window_number = int(cols[2].split('.')[-1])
-    return window_number, command
 
 def running_in_screen() -> bool:
     return os.getenv("STY") is not None
@@ -28,18 +22,19 @@ def get_current_window() -> int:
 def get_current_screen_name() -> str:
     return os.getenv("STY")
 
+def get_pid_cwd(pid: int) -> str:
+    return ""
+
 def save():
     screen_name = "".join(get_current_screen_name().split('.')[1:])
     script_name = args.script_name if args.script_name is not None else screen_name + ".sh"
 
-    # increase PROCPS_FROMLEN to ensure it is not cutoff as we use this to assocaite process to window.
-    res = subprocess.check_output(['PROCPS_FROMLEN=32', 'w'])
-    res = str(res)
-    # break up res into lines, remove the first line which is a header
-    res = res.split('\\n')[2:-1]
-    res = [parse_w_line(r) for r in res]
-    res.sort(key=lambda x : x[0])
+    with open('/var/run/utmp', 'rb') as f:
+        buf = f.read()
+        for entry in utmp.read(buf):
+            print(entry)
 
+    res = []
     if len(res) <= 0:
         raise SystemError("screen-saver must be run inside of a GNU-screen")
 
