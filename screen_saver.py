9387038479546d6202850948a593adce68cd9053
Felix Ansell <fansell@bitfirenetworks.com>
2025-04-20 23:03:09 -0400

    Window names.

--- screen_saver.py
+++ screen_saver.py
@@ -17,10 +17,20 @@ def running_in_screen() -> bool:
     return os.getenv("STY") is not None
 
 def get_window_names() -> Dict[int, str]:
-    res = subprocess.check_output(['screen', '-Q', 'windows'])
+    """
+    TODO:
+    screen -Q windows is not a reliable way to get tab title informtion as it is affected by window size and
+    only displays the first few tabs. So for now window titles are best effort.
+    """
+    process = subprocess.Popen(['screen', '-Q', 'windows'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout_data = process.stdout.read()
     res = res.split()
     res = {int(res[i]):res[i + 1] for i in range(0, res, 2)}
-    return res
+    return {}
+
+def get_window_name(window: int) -> str:
+     res = subprocess.check_output(['screen', '-p', str(window), '-Q', 'title']).decode("utf-8")
+     return res
 
 def get_current_window() -> int:
     res = subprocess.check_output(['screen', '-Q', 'number']).split()[0]
@@ -38,14 +48,14 @@ def save():
     # break up res into lines, remove the first line which is a header
     res = res.split('\\n')[2:-1]
     res = [parse_w_line(r) for r in res]
-    window_names = get_window_names()
+    #window_names = get_window_names()
     
     # build the bash script
     f = open(script_name, "w")
     f.write("#!/bin/sh\nscreen -dmS {0}\n".format(screen_name))
     for window, command in res:
         if window != 0:
-            f.write("screen -S {0} -X screen -t {1}\n".format(screen_name, window_names[window] if window in window_names else window))
+            f.write("screen -S {0} -X screen -t {1}\n".format(screen_name, get_window_name(window)))
         f.write("screen -S {0} -p {1} -X stuff {2}\r\n".format(screen_name, window, command))
 
     f.close()
