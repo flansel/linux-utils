9434400b2f99ac4ce712ffb228d43f0c9ac4ddb1
Felix Ansell <fansell@bitfirenetworks.com>
2025-04-21 00:13:13 -0400

    Fix file name

--- screen_saver.py
+++ screen_saver.py
@@ -1,6 +1,7 @@
 import subprocess
 import argparse
 import os
+import sys
 from typing import Tuple, Dict
 
 parser = argparse.ArgumentParser()
@@ -16,18 +17,6 @@ def parse_w_line(line: str) -> Tuple[int, str]:
 def running_in_screen() -> bool:
     return os.getenv("STY") is not None
 
-def get_window_names() -> Dict[int, str]:
-    """
-    TODO:
-    screen -Q windows is not a reliable way to get tab title informtion as it is affected by window size and
-    only displays the first few tabs. So for now window titles are best effort.
-    """
-    process = subprocess.Popen(['screen', '-Q', 'windows'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout_data = process.stdout.read()
-    res = res.split()
-    res = {int(res[i]):res[i + 1] for i in range(0, res, 2)}
-    return {}
-
 def get_window_name(window: int) -> str:
      res = subprocess.check_output(['screen', '-p', str(window), '-Q', 'title']).decode("utf-8")
      return res
@@ -40,21 +29,24 @@ def get_current_screen_name() -> str:
     return os.getenv("STY")
 
 def save():
-    screen_name = get_current_screen_name()
-    script_name = args.script_name if args.script_name is not None else screen_name
+    screen_name = "".join(get_current_screen_name().split('.')[1:])
+    script_name = args.script_name if args.script_name is not None else screen_name + ".sh"
 
     res = subprocess.check_output(['w'])
     res = str(res)
     # break up res into lines, remove the first line which is a header
     res = res.split('\\n')[2:-1]
     res = [parse_w_line(r) for r in res]
-    #window_names = get_window_names()
-    
+    res.sort(key=lambda x : x[0])
+
+    if len(res) <= 0:
+        raise SystemError("screen-saver must be run inside of a GNU-screen")
+
     # build the bash script
     f = open(script_name, "w")
-    f.write("#!/bin/sh\nscreen -dmS {0}\n".format(screen_name))
-    for window, command in res:
-        if window != 0:
+    f.write("#!/bin/sh\nscreen -dmS {0} -t {1}\n".format(screen_name, get_window_name(res[0][0])))
+    for idx, (window, command) in enumerate(res):
+        if idx != 0:
             f.write("screen -S {0} -X screen -t {1}\n".format(screen_name, get_window_name(window)))
         f.write("screen -S {0} -p {1} -X stuff {2}\r\n".format(screen_name, window, command))
 
