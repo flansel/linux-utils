8c8d08cccfb28e29bbd2cc4472b65eb31c74e8b2
Felix Ansell <fansell@bitfirenetworks.com>
2025-04-22 02:46:06 -0400

    Use utmp and psutil

--- screen_saver.py
+++ screen_saver.py
@@ -2,6 +2,8 @@ import subprocess
 import argparse
 import os
 import utmp
+import re
+import psutil
 from typing import Tuple, Dict
 
 parser = argparse.ArgumentParser()
@@ -25,31 +27,46 @@ def get_current_screen_name() -> str:
 def get_pid_cwd(pid: int) -> str:
     return ""
 
-def save():
-    screen_name = "".join(get_current_screen_name().split('.')[1:])
-    script_name = args.script_name if args.script_name is not None else screen_name + ".sh"
-
+def save(screen_name, script_name):
+    res = []
     with open('/var/run/utmp', 'rb') as f:
         buf = f.read()
         for entry in utmp.read(buf):
-            print(entry)
-
-    res = []
+            if entry.type == utmp.UTmpRecordType.user_process and re.match(".*::S\.[0-9]*$", entry.host):
+                res.append((int(entry.host.split(".")[-1]), entry.pid))
+    
     if len(res) <= 0:
         raise SystemError("screen-saver must be run inside of a GNU-screen")
 
+    res2 = []
+    for window, pid in res:
+        proc = psutil.Process(pid)
+        if len(proc.children()) > 0:
+            proc2 = proc.children()[0]
+            res2.append((window, " ".join(proc2.cmdline()), proc2.cwd()))
+        else:
+            res2.append((window, " ".join(proc.cmdline()), proc.cwd()))
+
+    res2.sort(key= lambda x: x[0])
+    current_window = get_current_window()
     # build the bash script
     f = open(script_name, "w")
-    f.write("#!/bin/sh\nscreen -dmS {0} -t {1}\n".format(screen_name, get_window_name(res[0][0])))
-    for idx, (window, command) in enumerate(res):
+    f.write("#!/bin/sh\nscreen -dmS {0} -t {1}\n".format(screen_name, get_window_name(res2[0][0])))
+    for idx, (window, command, cwd) in enumerate(res2):
         if idx != 0:
+            # the first tab is opened and named by the screen -dmS command
             f.write("screen -S {0} -X screen -t {1}\n".format(screen_name, get_window_name(window)))
-        f.write("screen -S {0} -p {1} -X stuff {2}\r\n".format(screen_name, window, command))
-
+        if window != current_window:
+            # don't run screen-saver again in the script
+            f.write("screen -S {0} -p {1} -X stuff {2}\r\n".format(screen_name, window, command))
+    
     f.close()
     os.chmod(script_name, 0o775)
 
 if __name__ == "__main__":
     if not running_in_screen():
         raise SystemError("screen-saver must be run inside of a GNU-screen.")
-    save()
+    
+    screen_name = "".join(get_current_screen_name().split('.')[1:])
+    script_name = args.script_name if args.script_name is not None else screen_name + ".sh"
+    save(screen_name, script_name)
